29422f51dba78da9d2af02fdc3eb3ea2634b6143
Indrayuda537 <indrayuda537@gmail.com>
2025-02-26 17:18:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Mendefinisikan variabel dengan tipe data integer dan string
+$angka = 10;
+$teks = "Pengulangan ke-";
+
+// Melakukan pemanggilan secara berulang menggunakan perulangan for
+for ($i = 1; $i <= $angka; $i++) {
+    echo "$teks $i <br><br>";
+}
+?>
